db91083e0ecc1f4922f463f11233f20124de8578
iftyzubaer <iftyzubaer1304.com>
2023-11-06 23:23:01 +0600

    created the search feature

--- src/Pages/AvailableFoods.jsx
+++ src/Pages/AvailableFoods.jsx
@@ -6,7 +6,8 @@ const AvailableFoods = () => {
 
     const foods = useLoaderData()
 
-    const [sortedFoods, setSortedFoods] = useState()
+    const [sortedFoods, setSortedFoods] = useState(null)
+    const [searchInput, setSearchInput] = useState('');
 
     const handleSort = () => {
         const sorted = [...foods].sort((a, b) => {
@@ -15,15 +16,25 @@ const AvailableFoods = () => {
         setSortedFoods(sorted)
     }
 
+    const handleSearch = () => {
+        if (searchInput) {
+            const filteredFoods = foods.filter((food) => food.name.toLowerCase().includes(searchInput.toLowerCase()))
+            setSortedFoods(filteredFoods)
+        }
+        else {
+            handleSort()
+        }
+    }
+
     return (
         <div className="px-2 md:px-20 lg:px-40 my-16 text-center">
             <h2 className="text-5xl font-bold text-center mb-10">Available Foods</h2>
             <div className="mb-10">
                 <div className="join">
-                    <input className="input input-bordered join-item" placeholder="Search" />
-                    <button className="btn join-item rounded-r-lg">Search</button>
+                    <input className="input input-bordered join-item" placeholder="Search" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+                    <button onClick={handleSearch} className="btn join-item rounded-r-lg">Search</button>
                 </div>
-                <button onClick={handleSort} className="btn rounded-lg ml-10">Sort</button>
+                <button onClick={handleSort} className="btn rounded-lg ml-10">Sort by Expiry Date</button>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
