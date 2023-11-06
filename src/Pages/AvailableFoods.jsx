b0014604f0751c3cae722649558183dbabca6ed2
iftyzubaer <iftyzubaer1304.com>
2023-11-06 23:12:50 +0600

    added the sort by expiry date feature

--- src/Pages/AvailableFoods.jsx
+++ src/Pages/AvailableFoods.jsx
@@ -1,15 +1,33 @@
 import { useLoaderData } from "react-router-dom";
 import AvailableFoodsCard from "./AvailableFoodsCard";
+import { useState } from "react";
 
 const AvailableFoods = () => {
 
     const foods = useLoaderData()
 
+    const [sortedFoods, setSortedFoods] = useState()
+
+    const handleSort = () => {
+        const sorted = [...foods].sort((a, b) => {
+            return new Date(a.expiryDate) - new Date(b.expiryDate)
+        })
+        setSortedFoods(sorted)
+    }
+
     return (
-        <div className="px-2 md:px-20 lg:px-40 my-16">
-            <h2 className="text-5xl font-bold text-center my-4">Available Foods</h2>
+        <div className="px-2 md:px-20 lg:px-40 my-16 text-center">
+            <h2 className="text-5xl font-bold text-center mb-10">Available Foods</h2>
+            <div className="mb-10">
+                <div className="join">
+                    <input className="input input-bordered join-item" placeholder="Search" />
+                    <button className="btn join-item rounded-r-lg">Search</button>
+                </div>
+                <button onClick={handleSort} className="btn rounded-lg ml-10">Sort</button>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
+                    sortedFoods ? sortedFoods.map(foods => <AvailableFoodsCard key={foods._id} foods={foods}></AvailableFoodsCard>) :
                     foods.map(foods => <AvailableFoodsCard key={foods._id} foods={foods}></AvailableFoodsCard>)
                 }
             </div>
